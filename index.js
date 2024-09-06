9deff5a60909a4ae1fc37923a0637d03ac6ff896
jaydeeptalsania <jaydeep@technerdssolutions.com>
2024-09-06 20:04:56 +0530

    change in index file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT  || 3000;
 
 app.get("/",(req,res)=>{
-   res.send("<h1>Welcome to node test app</h1>");
+   res.send("<h1>Welcome to node test app1</h1>");
 });
 
 app.listen(port,()=>{
