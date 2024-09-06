de083f986d61af8b760350a56436211a0826d40d
jaydeeptalsania <jaydeep@technerdssolutions.com>
2024-09-06 20:26:00 +0530

    handle webhook request

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const port = process.env.PORT  || 3000;
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-   console.log(req.body);
    res.send("<h1>Welcome to node test app1</h1>");
+});
+
+app.post("/",(req,res)=>{
+  console.log('Received webhook data:', req.body);
+  res.status(200).send('Webhook received');
 });
 
 app.listen(port,()=>{
